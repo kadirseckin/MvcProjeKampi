7fc7486c7fbee5dde1d8254b1e280f7d4d70cc23
Kadir Doğuş Seçkin <kadir.seckin44@gmail.com>
2021-07-22 15:00:50 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        private GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+
+            return repo.List();
+        }
+
+        public void CategoryAddBL(Category p)
+        {
+            if(p.CategoryName=="" || p.CategoryName.Length <= 3 || p.CategoryDescription == "")
+            {
+                //hata
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+
+    }
+}
---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        [Key]
+        public int AboutID { get; set; }
+
+        [StringLength(1000)]
+        public string AboutDetailsFirst { get; set; }
+
+        [StringLength(1000)]
+        public string AboutDetailsSecond { get; set; }
+
+        [StringLength(100)]
+        public string AboutImageFirst { get; set; }
+
+        [StringLength(100)]
+        public string AboutImageSecond { get; set; }
+
+    }
+}
